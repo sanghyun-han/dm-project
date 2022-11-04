f1a113f27e27bee0c98b6f91dd15c1ffb16d7ae3
Sanghyun Han <sanghyun@snu.ac.kr>
2022-11-04 19:08:28 +0900

    data

--- dataset.sh
+++ dataset.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+# Movielens
+wget https://files.grouplens.org/datasets/movielens/ml-25m.zip
 
-https://files.grouplens.org/datasets/movielens/ml-25m.zip
+mkdir ./data
+mv ml-25m.zip
+pushd ./data
+unzip ml-25m.zip
+popd
